7a3489599bfe58a5cdc17ba307b22e4944d30b22
LSerghei <36624444+LSerghei@users.noreply.github.com>
2018-11-26 21:34:31 +0200

    First Commit
    Added Unity Project

---
+++ FromGroundVR/Assets/AutoDoor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour {
+
+	private Component CurDoor;
+
+	public bool DoorOpen;
+	public bool PlayerNear; 
+	public float doorOpenAngle = 90f;
+    public float doorCloseAngle = 0f;
+    public float smooth = 5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		DoorOpen = false;
+		PlayerNear = false;
+
+		foreach (Component comp in transform){
+			if (comp.name == "Plane_001")
+				CurDoor = comp;
+
+		}
+		Debug.Log(CurDoor);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (PlayerNear){
+			if (!DoorOpen) {
+				Quaternion targetRotation = Quaternion.Euler (0, 0, doorOpenAngle);
+				CurDoor.transform.localRotation = Quaternion.Slerp (CurDoor.transform.localRotation, targetRotation, smooth * Time.deltaTime);
+
+				if (CurDoor.transform.localRotation.z > doorOpenAngle) {
+					DoorOpen = true;
+				}
+			}
+		} else {
+			if (DoorOpen) {
+
+				Quaternion targetRotation2 = Quaternion.Euler (0, 0, doorCloseAngle);
+				CurDoor.transform.localRotation = Quaternion.Slerp (CurDoor.transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+				if (CurDoor.transform.localRotation.z < doorCloseAngle) {
+					DoorOpen = false;			
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			PlayerNear = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			PlayerNear = false;
+		}
+	}
+}
---
+++ FromGroundVR/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	
+	//private Rigidbody rigid;
+	private CharacterController fplayer;
+	private PlayerLog eventLog;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//rigid = GetComponent<Rigidbody>();
+		fplayer = GetComponent<CharacterController>();
+		eventLog = GetComponent<PlayerLog>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+        float Horizontal = Input.GetAxis("Horizontal");
+		float Vertical = Input.GetAxis("Vertical");
+
+		Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
+
+		fplayer.Move(move * speed);
+		//rigid.AddForce(move * speed);
+
+		if (Input.GetKey(KeyCode.A))
+			eventLog.AddEvent("Player Moves Left");
+
+		if (Input.GetKey(KeyCode.W))
+			eventLog.AddEvent("Player Moves Right");
+	}
+}
