4b28d1ce58f418862b0aad1974644015e3bd2d32
Serghei Lealin <serghei.lealin@gmail.com>
2018-11-29 17:58:40 +0200

    Added Light and Lift

--- FromGroundVR/Assets/SlideDoor.cs
+++ FromGroundVR/Assets/SlideDoor.cs
@@ -4,49 +4,62 @@ using UnityEngine;
 
 public class SlideDoor : MonoBehaviour {
 
-	public Transform doorTransform;
-    public float raiseHeight = 3f;
+	public Transform doorTransform1, doorTransform2;
+    public float raiseHeight = 1f;
     public float speed = 3f;
-    private Vector3 _closedPosition;
+    private Vector3 _closedPosition1, _closedPosition2;
 
     // Use this for initialization
     void Start () {
-		doorTransform = transform;
-        _closedPosition = transform.position;
+		foreach (Component comp in transform){
+			if (comp.name == "Door1")
+				doorTransform1 = comp.transform;
+			
+			if (comp.name == "Door2")
+				doorTransform2 = comp.transform;
+
+		}
+        _closedPosition1 = doorTransform1.position;
+		_closedPosition2 = doorTransform2.position;
     }
 
-	/*void Update(){
-
-		if (Input.GetKeyDown (KeyCode.Q)) {
-			StopCoroutine ("MoveDoor");
-			Vector3 endpos = _closedPosition - new Vector3 (raiseHeight, 0f, 0f);
-			StartCoroutine ("MoveDoor", endpos);
-		} else if (Input.GetKeyDown (KeyCode.E)) {
-			StopCoroutine ("MoveDoor");
-			StartCoroutine ("MoveDoor", _closedPosition);
-		}
-
-	}*/
-
     void OnTriggerEnter (Collider other) {
-        StopCoroutine("MoveDoor");
-		Vector3 endpos = _closedPosition - new Vector3(raiseHeight, 0f, 0f);
-        StartCoroutine("MoveDoor", endpos);
+        StopCoroutine("MoveDoor1");
+		Vector3 endpos = _closedPosition1 - new Vector3(raiseHeight, 0f, 0f);
+        StartCoroutine("MoveDoor1", endpos);
+		        
+		StopCoroutine("MoveDoor2");
+		endpos = _closedPosition2 + new Vector3(raiseHeight, 0f, 0f);
+        StartCoroutine("MoveDoor2", endpos);
     }
 
     void OnTriggerExit (Collider other) {
-        StopCoroutine("MoveDoor");
-        StartCoroutine("MoveDoor", _closedPosition);
+        StopCoroutine("MoveDoor1");
+        StartCoroutine("MoveDoor1", _closedPosition1);
+		
+        StopCoroutine("MoveDoor2");
+        StartCoroutine("MoveDoor2", _closedPosition2);
     }
     
 
-    IEnumerator MoveDoor (Vector3 endPos) {
+    IEnumerator MoveDoor1 (Vector3 endPos) {
         float t = 0f;
-        Vector3 startPos = doorTransform.position;
+        Vector3 startPos = doorTransform1.position;
         while (t < 1f) {
 		//while (doorTransform.position != endPos){
             t += Time.deltaTime * speed;
-			doorTransform.position = Vector3.MoveTowards(startPos, endPos, t);
+			doorTransform1.position = Vector3.MoveTowards(startPos, endPos, t);
+            yield return null;
+        }
+    }
+	
+    IEnumerator MoveDoor2 (Vector3 endPos) {
+        float t = 0f;
+        Vector3 startPos = doorTransform2.position;
+        while (t < 1f) {
+		//while (doorTransform.position != endPos){
+            t += Time.deltaTime * speed;
+			doorTransform2.position = Vector3.MoveTowards(startPos, endPos, t);
             yield return null;
         }
     }
