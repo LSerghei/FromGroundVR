75e813c3da1aa8c856fd2c00b3bcbeff397f1b55
Serghei Lealin <serghei.lealin@gmail.com>
2018-11-30 17:42:41 +0200

    almost end

---
+++ FromGroundVR/Assets/LightsOnOff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOnOff : MonoBehaviour {
+
+	public bool LightOn = true;
+	
+	private GameObject[] Lights;
+	private bool inLightOn = true;
+	// Use this for initialization
+	void Start () {
+		//Lights = this.transform.Find("Lamps").GetComponent<Component>();
+		Lights = GetComponents<GameObject>();
+		Debug.Log(Lights);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (inLightOn != LightOn)
+		//{
+		//	foreach(GameObject gam in Lights)
+		//	{
+		//		gam.GetComponent<Light>.enabled = !gam.enabled;
+		//		inLightOn = !inLightOn;
+		//	}
+		//}
+	}
+}
